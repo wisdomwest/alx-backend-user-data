ff338d3fff8001e103099cb6586f6625abc8f400
wisdomwest <harmonykoome@gmail.com>
2024-04-11 00:29:24 +0300

    h

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,12 +36,14 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     '''returns a connector to the database'''
-    return mysql.connector.connection.MySQLConnection(
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    )
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    con = mysql.connector.connect(user=user, password=password,
+                                  host=host, database=db_name)
+    return con
 
 
 class RedactingFormatter(logging.Formatter):
