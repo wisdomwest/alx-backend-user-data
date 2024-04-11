7703164b7ebb2e92dd91e13a0df27fe268e7c51d
wisdomwest <harmonykoome@gmail.com>
2024-04-11 16:17:49 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''Encrypts a password'''
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Check if a password is valid'''
+    return bcrypt.checkpw(password.encode(), hashed_password)
