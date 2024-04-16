5e1d43e95d308be0a261769d03521fd2af9b6668
wisdomwest <harmonykoome@gmail.com>
2024-04-16 17:31:58 +0300

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -43,3 +45,23 @@ class BasicAuth(Auth):
            ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials
+        """
+        if user_email is None or \
+           type(user_email) is not str or \
+           user_pwd is None or \
+           type(user_pwd) is not str:
+            return None
+        try:
+            valid = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in valid:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
