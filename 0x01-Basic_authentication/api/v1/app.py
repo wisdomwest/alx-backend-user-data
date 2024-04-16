122db5dfbedad4699b7a2119d8bf13436a2bd6e1
wisdomwest <harmonykoome@gmail.com>
2024-04-16 15:38:29 +0300

    task 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,13 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler
