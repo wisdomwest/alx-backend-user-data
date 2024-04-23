3c5e6c6a319c37f6b260f65fb48acddf385b625c
wisdomwest <harmonykoome@gmail.com>
2024-04-24 01:12:39 +0300

    task 19 update password thru api

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,19 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    '''update user password'''
+    email = request.form['email']
+    reset_token = request.form['reset_token']
+    password = request.form['new_password']
+
+    try:
+        AUTH.update_password(reset_token, password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
