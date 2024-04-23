c83e73a7bbf2c8c0f53ad0a1118c7d0e8140e9dd
wisdomwest <harmonykoome@gmail.com>
2024-04-23 16:15:12 +0300

    task 10 create session ids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,3 +42,14 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        '''create a session for a user with session id'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
